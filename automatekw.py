daf4260405dd97df56c88570ef226d545ed4463d
Kevin <kevin.tarvin@gmail.com>
2016-09-25 18:55:17 -0400

    refactoring idea into reusable code

--- automatekw.py
+++ automatekw.py
@@ -1,52 +1,57 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Sep 23 10:52:47 2016
-
 @author: ktarvin
 """
 
 import os
+import sys
 import nltk
 import pandas as pd
 
-try:
-    filepath = (os.path.dirname(os.path.abspath(__file__)))
-    df = pd.read_csv('%s\\translations_meta_description_all.csv' % (filepath))
-    data = []
-    for i in df.index:
-        if isinstance(df['MetaDescription1'][i], str):
-            sents = nltk.sent_tokenize(df['MetaDescription1'][i])
-            for sent in sents:
-                tokens = nltk.word_tokenize(sent)
-                tagged = nltk.pos_tag(tokens)
-                tagged_tokens = nltk.pos_tag(tokens)
-                chunkGram = r'''
-                Chunk: {<JJ.?><NN>}
-                {<JJ><NN.?>}
-                {<NN.?><NN.?>}
-                {<NN|NN.?><NN|NN.?><NN|NN.?>}
-                {<VBG><NN|NN.?><NN|NN.?>}
-                {<JJ|JJ.?><NN|NN.?><NN|NN.?>}
-                '''
-                chunkParser = nltk.RegexpParser(chunkGram)
-                chunked = chunkParser.parse(tagged_tokens)
-                for n in chunked:
-                    if isinstance(n, nltk.tree.Tree):               
-                        if n.label() == 'NP':
-                            pass
-                            #print(n)
-                        else:
-                            w = [x[0] for x in n]
-                            keyword = '%s %s' % (w[0], w[1])
-                            #print(keyword)
-                            data.append(keyword)
-                #data.append(tagged_tokens)
-        else:
-            pass
-    df2 = pd.Series(data)
-    df3 = pd.DataFrame(df2)
-    df3.columns = ['keywords']
-    print(df3.head())
-    df3.to_csv('translationsKeywords.csv')
-except Exception as e:
-    raise SystemExit(e)
+
+def parseKeywords(argv):
+    try:
+        filepath = (os.path.dirname(os.path.abspath(__file__)))
+        df = pd.read_csv('%s\\%s' % (filepath, argv[0]))
+        data = []
+        for i in df.index:
+            if isinstance(df['MetaDescription1'][i], str):
+                sents = nltk.sent_tokenize(df['MetaDescription1'][i])
+                for sent in sents:
+                    tokens = nltk.word_tokenize(sent)
+                    tagged_tokens = nltk.pos_tag(tokens)
+                    chunkGram = r'''
+                    Chunk: {<JJ.?><NN>}
+                    {<JJ><NN.?>}
+                    {<NN.?><NN.?>}
+                    {<NN|NN.?><NN|NN.?><NN|NN.?>}
+                    {<VBG><NN|NN.?><NN|NN.?>}
+                    {<JJ|JJ.?><NN|NN.?><NN|NN.?>}
+                    '''
+                    chunkParser = nltk.RegexpParser(chunkGram)
+                    chunked = chunkParser.parse(tagged_tokens)
+                    for n in chunked:
+                        if isinstance(n, nltk.tree.Tree):               
+                            if n.label() == 'NP':
+                                pass
+                            else:
+                                w = [x[0] for x in n]
+                                keyword = '%s %s' % (w[0], w[1])
+                                data.append(keyword)
+            else:
+                pass
+        ds = pd.Series(data)
+        df = pd.DataFrame(ds)
+        df.columns = ['keywords']
+        df.to_csv('%s' % (argv[1]))
+    except Exception as e:
+        raise SystemExit(e)
+
+
+if __name__ == "__main__":
+    try:
+        argv = sys.argv[1:]
+        parseKeywords(argv)
+    except Exception as e:
+        raise SystemExit('Exited in general Exception in main Try with %s' % (e))
