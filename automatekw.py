e96ce9deb10a8223f95b5015ddb22745caa5d79d
Kevin <kevin.tarvin@gmail.com>
2016-09-25 18:00:19 -0400

    Added new chunking pattern and relative fp

--- automatekw.py
+++ automatekw.py
@@ -5,30 +5,29 @@ Created on Fri Sep 23 10:52:47 2016
 @author: ktarvin
 """
 
+import os
 import nltk
-#from nltk import sent_tokenize
-#from nltk import word_tokenize
-from nltk import pos_tag
 import pandas as pd
 
-
-
-#chunkGram = r'''Chunk: {<JJ.?>*<NN.?>*<NN>?}'''
-#chunkParser = nltk.RegexParser(chunkGram)
-#chunked = chunkParser.parse(tagged)
-
 try:
-    fp = r'C:\Users\ktarvin\Desktop\Motionpoint\meta_description_all.csv'
-    df = pd.read_csv(fp)
-    #print(df['MetaDescription1'])
+    filepath = (os.path.dirname(os.path.abspath(__file__)))
+    df = pd.read_csv('%s\\translations_meta_description_all.csv' % (filepath))
     data = []
     for i in df.index:
         if isinstance(df['MetaDescription1'][i], str):
             sents = nltk.sent_tokenize(df['MetaDescription1'][i])
             for sent in sents:
                 tokens = nltk.word_tokenize(sent)
-                tagged_tokens = pos_tag(tokens)
-                chunkGram = r'''Chunk: {<JJ.?><NN>}'''
+                tagged = nltk.pos_tag(tokens)
+                tagged_tokens = nltk.pos_tag(tokens)
+                chunkGram = r'''
+                Chunk: {<JJ.?><NN>}
+                {<JJ><NN.?>}
+                {<NN.?><NN.?>}
+                {<NN|NN.?><NN|NN.?><NN|NN.?>}
+                {<VBG><NN|NN.?><NN|NN.?>}
+                {<JJ|JJ.?><NN|NN.?><NN|NN.?>}
+                '''
                 chunkParser = nltk.RegexpParser(chunkGram)
                 chunked = chunkParser.parse(tagged_tokens)
                 for n in chunked:
@@ -47,6 +46,7 @@ try:
     df2 = pd.Series(data)
     df3 = pd.DataFrame(df2)
     df3.columns = ['keywords']
-    df3.to_csv('mpKeywords.csv')
+    print(df3.head())
+    df3.to_csv('translationsKeywords.csv')
 except Exception as e:
     raise SystemExit(e)
